5be8db5120ecc485955c98113d84555614c8a189
LuaMartins <luamartins070@gmail.com>
2021-03-31 19:23:09 -0300

    Initial commit

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    //velocidade do player, rotação e a curva//
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 2f;
+    float rotSpeed = 3f;
+
+    //declaração de gameObjects//
+    public GameObject wpManager;
+    GameObject[] wp;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+
+    void Start()
+    {
+        //pegar componente WpManager//
+        wp = wpManager.GetComponent<WpManager>().waypoints;
+        g = wpManager.GetComponent<WpManager>().graph;
+
+        currentNode = wp[0]; //inicia no 0//
+    }
+
+
+
+    //metodo para ir ao heliport//
+    public void GotoHeliport()
+    {
+        g.AStar(currentNode, wp[1]);
+        currentWP = 0;
+    }
+
+    //metodo para ir para ruinas//
+    public void GotoRuinas()
+    {
+        g.AStar(currentNode, wp[6]);
+        currentWP = 0;
+    }
+
+    //metodo para ir para tanques//
+    public void Gototanques()
+    {
+        g.AStar(currentNode, wp[7]);
+        currentWP = 0;
+    }
+
+
+
+    private void LateUpdate()
+    {
+
+        //retorno para o 0//
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+
+        currentNode = g.getPathPoint(currentWP); //pegando o ponto do path//
+
+
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy) // distancia para as posições//
+        { currentWP++; }
+
+        if (currentWP < g.getPathLength()) 
+        {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z); //olhar e direção para o player seguir//
+            Vector3 direction = lookAtGoal - this.transform.position; // direção do lookgoal//
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed); // velocidade da rotação//
+            this.transform.Translate(0, 0, speed * Time.deltaTime); // velocidade x o deltatime//
+        }
+    }
+}
---
+++ Assets/WpManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Link
+{
+    // criando game objects para direcionar dentro da unity//
+    public enum direction { UNI, BI }
+    public GameObject node1;
+    public GameObject node2;
+    public direction dir;
+
+}
+public class WpManager : MonoBehaviour
+{
+    // declaração de gameObjects da classe WpManager//
+    public GameObject[] waypoints;
+    public Link[] links;
+    public Graph graph = new Graph();
+
+    void Start()
+    {
+        // startando os valores para o tanque seguir//
+        if (waypoints.Length > 0)
+        {
+            foreach (GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+            foreach (Link i in links)
+            {
+                graph.AddEdge(i.node1, i.node2);
+                if (i.dir == Link.direction.BI)
+                {
+                    graph.AddEdge(i.node1, i.node2);
+                }
+            }
+
+        }
+
+    }
+
+    
+    void Update()
+    {
+        graph.debugDraw(); // ver a linha na cena que busca do graph//
+    }
+}
