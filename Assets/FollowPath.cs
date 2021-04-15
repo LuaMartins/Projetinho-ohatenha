a05e69f54e09c8cfe7f6b13d834aa643342bf71d
LuaMartins <luamartins070@gmail.com>
2021-04-15 19:03:36 -0300

    att1504
    atualização do projeto com navmesh

--- Assets/FollowPath.cs
+++ Assets/FollowPath.cs
@@ -5,26 +5,28 @@ using UnityEngine;
 public class FollowPath : MonoBehaviour
 {
     //velocidade do player, rotação e a curva//
-    Transform goal;
-    float speed = 5.0f;
-    float accuracy = 2f;
-    float rotSpeed = 3f;
+   // Transform goal;
+  //  float speed = 5.0f;
+  //  float accuracy = 2f;
+   // float rotSpeed = 3f;
 
     //declaração de gameObjects//
     public GameObject wpManager;
     GameObject[] wp;
-    GameObject currentNode;
-    int currentWP = 0;
-    Graph g;
+    UnityEngine.AI.NavMeshAgent agent;
+
+   // GameObject currentNode;
+    //int currentWP = 0;
+   // Graph g;
 
 
     void Start()
     {
         //pegar componente WpManager//
         wp = wpManager.GetComponent<WpManager>().waypoints;
-        g = wpManager.GetComponent<WpManager>().graph;
-
-        currentNode = wp[0]; //inicia no 0//
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        //g = wpManager.GetComponent<WpManager>().graph;
+       // currentNode = wp[0]; //inicia no 0//
     }
 
 
@@ -32,29 +34,32 @@ public class FollowPath : MonoBehaviour
     //metodo para ir ao heliport//
     public void GotoHeliport()
     {
-        g.AStar(currentNode, wp[1]);
-        currentWP = 0;
+        agent.SetDestination(wp[1].transform.position);
+        //g.AStar(currentNode, wp[1]);
+        //currentWP = 0;
     }
 
     //metodo para ir para ruinas//
     public void GotoRuinas()
     {
-        g.AStar(currentNode, wp[6]);
-        currentWP = 0;
+        agent.SetDestination(wp[5].transform.position);
+        //g.AStar(currentNode, wp[6]);
+       // currentWP = 0;
     }
 
     //metodo para ir para tanques//
     public void Gototanques()
     {
-        g.AStar(currentNode, wp[7]);
-        currentWP = 0;
+        agent.SetDestination(wp[9].transform.position);
+        //g.AStar(currentNode, wp[7]);
+       // currentWP = 0;
     }
 
 
 
     private void LateUpdate()
     {
-
+        /* código utilizado na tarefa anterior
         //retorno para o 0//
         if (g.getPathLength() == 0 || currentWP == g.getPathLength())
             return;
@@ -73,6 +78,6 @@ public class FollowPath : MonoBehaviour
             Vector3 direction = lookAtGoal - this.transform.position; // direção do lookgoal//
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed); // velocidade da rotação//
             this.transform.Translate(0, 0, speed * Time.deltaTime); // velocidade x o deltatime//
-        }
+        }*/
     }
 }
